0b29dd78a7f1f50b9f6d43ad47024dd6a6f411fb
priya-123-debug <supriyakum880@gmail.com>
2026-07-24 23:01:55 +0530

    add new question of armstrong checking

---
+++ question3armstrongnumber.py
@@ -0,0 +1,15 @@
+#armstrong digit power of digit is same as no of digit 
+# 132 is 1*3+3*3+2*3 
+def armstrong(num):
+    total=0
+    ln=len(str(num))
+    while num>0:
+        id=num%10
+        total=total+(id**ln)
+        num=num//10
+    return total==num
+
+
+for i in range(5000,5020):
+    print(armstrong(i))
+
