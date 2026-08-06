a666182fb4da5f0258f6b4053d7cacd12e82ce82
priya-123-debug <supriyakum880@gmail.com>
2026-08-06 09:13:44 +0530

    add

---
+++ spiral_matrix.py
@@ -0,0 +1,27 @@
+matrix=[
+    [ 1, -7, -4,  0],
+    [ 2, -8,  0, -1],
+    [ 3,  0, -5, -2],
+    [ 0, -9, -6, -3]
+]
+result =[]
+top,left=0,0
+bottom,right=len(matrix)-1,len(matrix[0])-1
+while top<=bottom and left<=right:
+    for i in range(left,right+1):
+        result.append(matrix[top][i])
+    top+=1
+    for i in range(top,bottom+1):
+        result.append(matrix[i][right])
+    right-=1
+    if top<=bottom:
+        for i in range(right,left-1,-1):
+            result.append(matrix[bottom][i])
+        bottom-=1
+    if left<=right:
+        for i in range(bottom,top-1,-1):
+            result.append(matrix[i][left])
+        left+=1
+print(result)
+
+
