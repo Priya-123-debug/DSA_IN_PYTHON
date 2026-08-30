1b05f41b2521b94b55aa9562581b447474286595
priya-123-debug <supriyakum880@gmail.com>
2026-08-30 23:45:03 +0530

    add question 4sum

---
+++ foursum.py
@@ -0,0 +1,31 @@
+nums=[1,1,1,1,2,2,3,3,3,4,4,4,5,5] 
+n=len(nums)
+ans=[]
+nums.sort()
+target=8
+for i in range(0,n):
+    if i>0 and nums[i]==nums[i-1]:
+        continue
+    for j in range(i+1,n):
+        if j>i+1 and nums[j]==nums[j-1]:
+            continue
+        k=j+1
+        l=n-1
+        while k<l:
+            total=nums[i]+nums[j]+nums[k]+nums[l]
+            if total==target:
+                ans.append([nums[i],nums[j],nums[k],nums[l]])
+                k+=1
+                l-=1
+                while k<l and nums[k]==nums[k-1]:
+                    k+=1
+
+                while l>k and nums[l]==nums[l-1]:
+                    l-=1
+            elif total<target:
+                k+=1
+            else:
+                l-=1
+print(ans)
+
+
