8f4f645b93f2b9d9c6316900fe71cd489f7e4e31
priya-123-debug <supriyakum880@gmail.com>
2026-08-31 06:37:06 +0530

    add new code

---
+++ binarysearch.py
@@ -0,0 +1,15 @@
+nums=[2,4,6,7,9,11,18,19] 
+def binarysearch(nums,target):
+    n=len(nums)
+    low=0
+    high=n-1
+    while low<=high:
+        mid=(low+high)//2
+        if nums[mid]==target:
+            return mid
+        elif nums[mid]<target:
+            low=mid+1
+        else:
+            high=mid-1
+    return -1
+print(binarysearch(nums,9));
