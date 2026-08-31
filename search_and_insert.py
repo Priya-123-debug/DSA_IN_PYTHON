d25df4ac7eb04dfeabf12b89fbd9bc92b2194c16
priya-123-debug <supriyakum880@gmail.com>
2026-08-31 07:06:46 +0530

    add search and insert

---
+++ search_and_insert.py
@@ -0,0 +1,14 @@
+nums=[1,3,4,5,8,9,14,15,19,20,21]
+n=len(nums)
+low=0
+high=n-1
+target=6
+lb=-1
+while low<=high:
+    mid=(low+high)//2
+    if(nums[mid]>=target):
+        lb=mid
+        high=mid-1
+    else:
+        low=mid+1
+print(lb)
